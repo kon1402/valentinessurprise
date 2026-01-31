9a05dc60ec7d127a3a97bb3f25083a7db652145a
kon1402 <kon1402@bu.edu>
2026-01-31 15:52:01 -0500

    Valentine surprise with login

--- push.bat
+++ push.bat
@@ -1,10 +1,6 @@
 @echo off
 cd /d "%~dp0"
-git config user.email "kon1402@bu.edu"
-git config user.name "kon1402"
 git add .
-git commit -m "Valentine surprise with login"
-git branch -M main
-git remote add origin https://github.com/kon1402/valentinessurprise.git
-git push -u origin main
+git commit -m "Updates"
+git push
 pause
